8688206ffb776a3b4cdd2d2a8c5c9181ccfc5183
exp0nge <mdislamwork@gmail.com>
2016-12-12 00:50:45 -0500

    added print board

--- connect-4.scm
+++ connect-4.scm
@@ -1,3 +1,5 @@
+(#%require racket/vector)
+
 (define player-1 'X)
 
 (define player-2 'O)
@@ -14,26 +16,41 @@
             (init-vector-rows rows (+ count 1)))))) 
 
 (define init-board-matrix
-  ;; Each row-list contains that level's columns
-  ; ((() () () () () () ())
-  ; (() () () () () () ())
-  ; (() () () () () () ())
-  ; (() () () () () () ())
-  ; (() () () () () () ())
-  ; (() () () () () () ()))
+  ;; Vector contains columns vectors
+  ; #(#(() () () () () () ())
+  ; #(() () () () () () ())
+  ; #(() () () () () () ())
+  ; #(() () () () () () ())
+  ; #(() () () () () () ())
+  ; #(() () () () () () ()))
   (init-vector-rows (make-vector num-rows) 0))
 
 (define board init-board-matrix)
-
 
 (define drop-chip
   (lambda (col player)
     (define check-lowest-row
       (lambda (row)
         (cond
-          ((< row 0) (display "invalid move"))
+          ((< row 0) (display "invalid move") (newline))
           ((null? (vector-ref (vector-ref board row) col))
            (vector-set! (vector-ref board row) col player))
           (else (check-lowest-row (- row 1))))))
     (check-lowest-row (- num-rows 1))))
+
+(define print-board
+  (lambda ()
+    (define iter
+      (lambda (row)
+        (cond
+          ((= row num-rows) (display "0 1 2 3 4 5 6"))
+          (else
+           (vector-map (lambda (col) (cond
+                                       ((null? col) (display " |"))
+                                       (else (display col) (display "|"))))
+                       (vector-ref board row))
+           (newline)
+           (iter (+ row 1))))))
+    (iter 0)))
+        
 
